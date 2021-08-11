f276d265c6ea297d3e2259a193f1cbf3a9db2996
Daniel <narselldev@gmail.com>
2021-08-11 17:26:32 -0500

    Estructura inicial

---
+++ src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    users_api_url: process.env.USERS_API_URL || 'https://mision-tic-users-narsell.herokuapp.com/',
+    account_api_url: process.env.ACCOUNT_API_URL || 'https://cuentasmsnarsell.herokuapp.com/',
+    
+};
